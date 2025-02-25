7e691127f15c5a5c095d0ef51ca0788845a9662d
Emmanuel Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-02-25 01:23:39 -0300

    Development (#1)
    * chore:  the countries page was developed
    
    * chore:  developed each country's page
    
    * chore:  developed the about component
    
    * chore: developed the footer component

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
-import NavaBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -45,8 +46,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
   return (
     <>
-      <NavaBar />
+      <NavBar />
       <Outlet />
+      <Footer />
     </>
   );
 }
